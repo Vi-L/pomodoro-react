991bf02a298be7f366eea3171d43379f98ed325e
Vi-L <viclei1216@gmail.com>
2024-04-09 18:29:51 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [totalTimerLength, setTotalTimerLength] = useState(60_000 * 25)
+  const [durationMS, setDurationMS] = useState(60_000 * 25)
+  const [remainingTime, setRemainingTime] = useState(null)
+  const [startTime, setStartTime] = useState(null)
+  const [active, setActive] = useState(false)
+  function handleStart() { // make timer into a hook ?
+    if (!active) {
+      setStartTime(Date.now())
+      if (remainingTime > 0 && remainingTime !== null) {
+        setDurationMS(remainingTime)
+      }
+    }
+    if (remainingTime === 0) {
+      setRemainingTime(null)
+      setDurationMS(totalTimerLength)
+    }
+    setActive(prev => !prev)
+  }
+  function handleReset() {
+    setRemainingTime(null)
+    setActive(false)
+  }
+  function changeTime(newTime) {
+    handleReset()
+    setTotalTimerLength(newTime)
+    setDurationMS(newTime)
+  }
+  function formatMS() {
+    let time = remainingTime
+    if (remainingTime === null) {
+      time = durationMS
+    } else if (remainingTime === 0) {
+      time = remainingTime
+    } else {
+      time += 1000
+    }
+    return `${padTime(Math.floor(time / 1000 / 60))} : ${padTime(Math.floor(time / 1000 % 60))}`
+  }
+  function padTime(time) {
+    return time.toString().padStart(2, "0")
+  }
+
+  useEffect(() => {
+    if (active) {
+      if (remainingTime <= 1 && remainingTime !== null) {
+        setActive(false)
+        setRemainingTime(0) 
+      } else {
+        const timeoutID = setTimeout(() => {
+          setRemainingTime(durationMS - (Date.now() - startTime)) 
+        }, 50)
+        return () => { clearTimeout(timeoutID) } //cleanup function
+      }
+    }
+  }, [startTime, remainingTime, active])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleStart}>{active ? "Stop" : "Start"}</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={() => changeTime(60_000 * 25)}>Pomodoro</button>
+      <button onClick={() => changeTime(60_000 * 5)}>Short Break</button>
+      <button onClick={() => changeTime(60_000 * 15)}>Long Break</button>
+      <h1>{formatMS()}</h1>
     </div>
   );
+}
+
+function Button() {
+
+
 }
 
 export default App;
